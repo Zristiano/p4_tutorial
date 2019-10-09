2b1b6c69b9329708a9610467f9f7929e08ca5ea3
Zristiano <zengyuanmeng@gmail.com>
2019-10-09 03:53:44 -0500

    add bind_layer

--- exercises/milestone1/send.py
+++ exercises/milestone1/send.py
@@ -8,6 +8,7 @@ import struct
 from scapy.all import sendp, send, get_if_list, get_if_hwaddr
 from scapy.all import Packet
 from scapy.all import Ether, IP, UDP, TCP, ShortField
+from scapy.all import bind_layers
 
 class ECMP(Packet):
     name = "ECMP"
